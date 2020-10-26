a61975076e8f73f19b12dafec17a34fc5e24a830
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-10-26 06:50:18 -0700

    changes to post and fullpost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,30 +8,49 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null // selectedPostId pt1
     }
 
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0, 4); // fetching only 4 posts
+                const updatedPosts = posts.map(post => { // updating posts and transformation
+                    return {
+                        ...post,
+                        author: 'Abel'
+                    }
+                })
+                this.setState({posts: updatedPosts});
+                // this.setState({posts: response.data}); // 1st code
                 console.log(response);
                 
             });
     };
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id}); // selectedPostId pt2
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            <Post title={post.title} />
-        })
+           return <Post 
+            key={post.id} 
+            title={post.title} 
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)} // method passes id to handler 
+           />;
+        });
 
+        // selectedPostId pt3 in fullpost
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
